f4d90c202adb0f3936440522776583b185cd9214
Andrei Volcov <andrey.volcov@gmail.com>
2016-03-02 12:44:17 +0200

    Fix footer, form, and scope

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -11,8 +11,8 @@ class Transaction < ActiveRecord::Base
   after_destroy :affect_to_accounts_after_deletion
   after_update  :affect_to_accounts_after_update
 
-  scope :in_current_month, -> { where(created_at: Date.today.beginning_of_month..Date.today.end_of_month)}
-  scope :in_date, -> (date) { where(created_at: date..date.end_of_month)}
+  scope :in_current_month, -> { where(created_at: Date.today.beginning_of_month-1.day..Date.today.end_of_month)}
+  scope :in_date, -> (date) { where(created_at: date-1.day..date.end_of_month)}
   scope :account, -> (account_id) { where(account_id: account_id) }
   scope :category, -> (category_id) { where(category_id: category_id) }
 
