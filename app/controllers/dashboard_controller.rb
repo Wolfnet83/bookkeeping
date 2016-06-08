89ce9b5998cb172797995c04332c50cf0e492604
Andrei Volcov <andrey.volcov@gmail.com>
2016-06-08 10:50:08 +0300

    Add top categories to dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,5 +2,14 @@ class DashboardController < ApplicationController
   before_action :authenticate_user!
   def index
     @accounts = current_user.accounts.all
+    calculate_top_categories
+  end
+
+  private
+  def calculate_top_categories
+    @categories = Transaction.select('category_id as cat, sum(amount) as am')
+                             .in_current_month.group(:category_id)
+                             .order("sum(amount) desc")
+                             .limit(10)
   end
 end
