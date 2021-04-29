620d779fffffc29bedaf3ade7a03831e7423ee6e
Andrei Volcov <andrey.volcov@gmail.com>
2021-04-29 10:34:57 +0300

    Add total in reports

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,5 +1,6 @@
 class ReportsController < ApplicationController
   def top_categories
+    @total = 0
     if params[:date].present?
       @date = "#{params[:date][:month]}/#{params[:date][:year]}".to_date
       @categories = Transaction.select('category_id as cat, sum(amount) as am, sum(amount_in_dc) as am_in_dc')
@@ -8,6 +9,8 @@ class ReportsController < ApplicationController
                              .group(:category_id)
                              .order("sum(amount_in_dc) desc")
                              .in_date(@date)
+
+      @categories.each{ |cat| @total+=cat.am_in_dc}
     else
       @date = Date.today.beginning_of_month
       @categories = Transaction.select('category_id as cat, sum(amount) as am, sum(amount_in_dc) as am_in_dc')
@@ -16,9 +19,12 @@ class ReportsController < ApplicationController
                              .group(:category_id)
                              .order("sum(amount_in_dc) desc")
                              .in_current_month
+      @categories.each{ |cat| @total+=cat.am_in_dc}
     end
   end
+
   def top_categories_by_year
+    @total = 0
     if params[:date].present?
       @date = "01/#{params[:date][:year]}".to_date
     else
@@ -30,5 +36,6 @@ class ReportsController < ApplicationController
                            .group(:category_id)
                            .order("sum(amount_in_dc) desc")
                            .in_year(@date)
+      @categories.each{ |cat| @total+=cat.am_in_dc}
   end
 end
