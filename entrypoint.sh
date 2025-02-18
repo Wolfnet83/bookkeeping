57a9f6e9388bca777ac00f6ca3fbacd1ffd20ddf
Andrei Volcov <andrey.volcov@gmail.com>
2025-02-18 14:37:05 +0200

    comment db actions on entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,9 +4,9 @@ set -e
 # Remove a potentially pre-existing server.pid for Rails.
 rm -f /bookke/tmp/pids/server.pid
 
-rails db:create
-rails db:migrate
-rails db:seed
+# rails db:create
+# rails db:migrate
+# rails db:seed
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
